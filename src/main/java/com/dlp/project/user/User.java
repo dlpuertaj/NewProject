688442b51aea5009213e4b4c2b04c631161e6599
David Leonardo <dlpuerta@gmail.com>
2022-05-20 20:37:55 -0500

    Created User entity

---
+++ src/main/java/com/dlp/project/user/User.java
@@ -0,0 +1,26 @@
+package com.dlp.project.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+}

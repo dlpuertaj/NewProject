e94c63df8f5bf70d100a8f66dfac653183851791
David Leonardo <dlpuerta@gmail.com>
2022-06-01 20:21:52 -0500

    BasicAuth

--- src/main/java/com/dlp/project/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/dlp/project/configuration/WebSecurityConfiguration.java
@@ -2,15 +2,19 @@ package com.dlp.project.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/**").permitAll()
-                //.antMatchers(HttpMethod.POST,"/app/v1/user").permitAll()
-                .anyRequest().authenticated();
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
